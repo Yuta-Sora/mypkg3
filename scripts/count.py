2fc052fe9fb479dc3226a9abaab655effb046c0a
Yuta Isobe <ubuntu@ubuntu.mshome.net>
2020-01-24 04:37:40 +0900

    Add new mode

--- scripts/count.py
+++ scripts/count.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python
 import rospy
 from std_msgs.msg import Int32
+from std_msgs.msg import String
 
 rospy.init_node('count')
 pub = rospy.Publisher('count_up', Int32, queue_size=1)
+pub2 = rospy.Publisher('count_2', String, queue_size=10)
+pub3 = rospy.Publisher('count_3', String, queue_size=10)
 rate = rospy.Rate(10)
 n = 0
+a = 99
+word = "Hello World"
 while not rospy.is_shutdown():
     n += 1
+    word2 = raw_input()
     pub.publish(n)
+    pub2.publish(word)
+    pub3.publish(word2)
     rate.sleep()
 
--- scripts/twice.py
+++ scripts/twice.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python
 import rospy
 from std_msgs.msg import Int32
+from std_msgs.msg import String
 
 n = 0
 
 def cb(message):
     global n
     n = message.data*2
+def cb2(message):
+    rospy.loginfo(message.data)
+
+def cb3(message):
+    rospy.loginfo(message.data)
 
 if __name__ == '__main__':
     rospy.init_node('twice')
     sub = rospy.Subscriber('count_up', Int32, cb)
+    sub3 = rospy.Subscriber('count_3', String, cb3)
     pub = rospy.Publisher('twice', Int32, queue_size=1)
     rate = rospy.Rate(10)
     while not rospy.is_shutdown():
